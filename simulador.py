af652a0f30b030f1f3e585a0f04d62fd2a0e64cc
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-02 17:41:11 -0300

    Definindo as variaveis de estado e os eventos do simulador

---
+++ simulador.py
@@ -0,0 +1,13 @@
+# Definir variáveis de estados:
+# -- numero de pessoas na esteira -> quantidade_esteira
+# -- numero de pessoas na bike -> quantidade_bike
+
+
+# Definir eventos:
+# -- chegada de pessoas na academia -> chegada_academia
+# -- saída de pessoas depois da esteira -> saida_esteira
+# -- entrada de pessoa na bike -> entrada_bike
+# -- saída de pessoas depois da bike -> saida_bike
+# -- reentrada na esteira -> reentrada_esteira
+
+
